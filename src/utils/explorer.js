dbd2271094978656198fac32407fac469ad62cc8
Dave86dev <davidochando1986@gmail.com>
2024-02-04 01:35:42 +0100

    feat: search working, still need to display results

--- src/utils/explorer.js
+++ src/utils/explorer.js
@@ -1,4 +1,31 @@
 export const explorer = (json, criteria) => {
-    console.log("criteria received ok", criteria)
-    return true 
+    
+    //Recursive
+    const recursiveSearch = (json, depth = 0, actualRoute = "") => {
+        
+        if(Array.isArray(json)){
+            //Arrays loop.
+            for(let i = 0; i < json.length; i++){
+                const result = recursiveSearch(json[i], depth + 1, `${actualRoute}[${i}]`)
+                if (result) return result
+            }
+
+        } else if (typeof(json) === "object" && json !== null) {
+            //Objects loops, looking for our target.
+            for(const [key, val] of Object.entries(json)){
+                const newPath = actualRoute ? `${actualRoute}.${key}` : key
+                if(newPath === criteria){
+                    //bingo! we have got it
+                    return val
+                }
+            const result = recursiveSearch(val, depth + 1, newPath)
+            if (result) return result
+            }
+            
+        }
+
+        return undefined
+    }
+
+    return recursiveSearch(json)
 }
